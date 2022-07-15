fd47740a03f8885f3aed69f645200acd00879234
pranav-bharadwaj <pranavbharadwaj2001@gmail.com>
2022-07-15 13:52:23 +0530

    updated bus stops added

--- lib/UI/busDetails.dart
+++ lib/UI/busDetails.dart
@@ -10,6 +10,22 @@ class BusDetails extends StatefulWidget {
 
 class _BusDetailsState extends State<BusDetails> {
   bool isSwitched = false;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _nameController = TextEditingController();
+  static List<String> friendsList = [""];
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
@@ -128,129 +144,11 @@ class _BusDetailsState extends State<BusDetails> {
                             ),
                           ),
                         ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        ..._getFriends(),
                         SizedBox(height: 25),
-                        TextFormField(
-                          cursorColor: Colors.red,
-                          // controller: emailEditingController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return ("Please Enter Your Email");
-                            }
-                            // reg expression for email validation
-                            if (!RegExp(
-                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                                .hasMatch(value)) {
-                              return ("Please Enter a valid email");
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            // emailEditingController.text = value!;
-                          },
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 16),
-                          decoration: InputDecoration(
-                            labelText: "From Station",
-                            labelStyle: TextStyle(
-                                color: kAccentColor,
-                                fontWeight: FontWeight.normal),
-                            isDense: true,
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.only(
-                                  left: 10, right: 3, bottom: 3),
-                              child: Icon(
-                                Icons.bus_alert,
-                                size: 25,
-                                color: Theme.of(context).brightness ==
-                                        Brightness.light
-                                    ? Colors.black26
-                                    : Colors.white24,
-                              ),
-                            ),
-                            hintText: "Enter Starting station",
-                            hintStyle: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontFamily: "OpenSans",
-                              color: Theme.of(context).brightness ==
-                                      Brightness.light
-                                  ? Colors.black26
-                                  : Colors.white24,
-                            ),
-                            border: OutlineInputBorder(),
-                            prefixIconConstraints:
-                                BoxConstraints(minWidth: 0, minHeight: 0),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                              borderSide:
-                                  BorderSide(color: Colors.red, width: 2.0),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 25,
-                        ),
-                        TextFormField(
-                          cursorColor: Colors.red,
-                          // controller: emailEditingController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return ("Please Enter Your Email");
-                            }
-                            // reg expression for email validation
-                            if (!RegExp(
-                                    "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                                .hasMatch(value)) {
-                              return ("Please Enter a valid email");
-                            }
-                            return null;
-                          },
-                          onSaved: (value) {
-                            // emailEditingController.text = value!;
-                          },
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 16),
-                          decoration: InputDecoration(
-                            labelText: "To Station",
-                            labelStyle: TextStyle(
-                                color: kAccentColor,
-                                fontWeight: FontWeight.normal),
-                            isDense: true,
-                            prefixIcon: Padding(
-                              padding: EdgeInsets.only(
-                                  left: 10, right: 3, bottom: 3),
-                              child: Icon(
-                                Icons.gps_fixed,
-                                size: 25,
-                                color: Theme.of(context).brightness ==
-                                        Brightness.light
-                                    ? Colors.black26
-                                    : Colors.white24,
-                              ),
-                            ),
-                            hintText: "Enter Bus Destination",
-                            hintStyle: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontFamily: "OpenSans",
-                              color: Theme.of(context).brightness ==
-                                      Brightness.light
-                                  ? Colors.black26
-                                  : Colors.white24,
-                            ),
-                            border: OutlineInputBorder(),
-                            prefixIconConstraints:
-                                BoxConstraints(minWidth: 0, minHeight: 0),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8.0),
-                              borderSide:
-                                  BorderSide(color: Colors.red, width: 2.0),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 25,
-                        ),
                         Container(
                           width: screenWidth,
                           child: ElevatedButton(
@@ -259,12 +157,140 @@ class _BusDetailsState extends State<BusDetails> {
                             },
                             child: Text('Update Bus Details'),
                           ),
-                        )
+                        ),
                       ],
                     ))
               ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _getFriends() {
+    List<Widget> friendsTextFields = [];
+    for (int i = 0; i < friendsList.length; i++) {
+      friendsTextFields.add(Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Row(
+          children: [
+            Expanded(child: FriendTextFields(i)),
+            SizedBox(
+              width: 16,
+            ),
+            // we need add button at last friends row
+            _addRemoveButton(i == friendsList.length - 1, i),
+          ],
+        ),
+      ));
+    }
+    return friendsTextFields;
+  }
+
+  /// add / remove button
+  Widget _addRemoveButton(bool add, int index) {
+    return InkWell(
+      onTap: () {
+        if (add) {
+          // add new text-fields at the top of all friends textfields
+          friendsList.insert(0, "");
+        } else
+          friendsList.removeAt(index);
+        setState(() {});
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: (add) ? Colors.green : Colors.red,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Icon(
+          (add) ? Icons.add : Icons.remove,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class FriendTextFields extends StatefulWidget {
+  final int index;
+  FriendTextFields(this.index);
+  @override
+  _FriendTextFieldsState createState() => _FriendTextFieldsState();
+}
+
+class _FriendTextFieldsState extends State<FriendTextFields> {
+  TextEditingController _nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _nameController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _nameController.text = _BusDetailsState.friendsList[widget.index];
+    });
+
+    return TextFormField(
+      controller: _nameController,
+      onChanged: (v) => _BusDetailsState.friendsList[widget.index] = v,
+      cursorColor: Colors.red,
+      // controller: emailEditingController,
+      keyboardType: TextInputType.emailAddress,
+      validator: (value) {
+        if (value!.isEmpty) {
+          return ("Please Enter Your Email");
+        }
+        // reg expression for email validation
+        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]").hasMatch(value)) {
+          return ("Please Enter a valid email");
+        }
+        return null;
+      },
+      onSaved: (value) {
+        // emailEditingController.text = value!;
+      },
+      style: TextStyle(fontWeight: FontWeight.w500, fontSize: 16),
+      decoration: InputDecoration(
+        labelText: "Bus Stops",
+        labelStyle:
+            TextStyle(color: kAccentColor, fontWeight: FontWeight.normal),
+        isDense: true,
+        prefixIcon: Padding(
+          padding: EdgeInsets.only(left: 10, right: 3, bottom: 3),
+          child: Icon(
+            Icons.gps_fixed,
+            size: 25,
+            color: Theme.of(context).brightness == Brightness.light
+                ? Colors.black26
+                : Colors.white24,
+          ),
+        ),
+        hintText: "Enter New Bus Stop",
+        hintStyle: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontFamily: "OpenSans",
+          color: Theme.of(context).brightness == Brightness.light
+              ? Colors.black26
+              : Colors.white24,
+        ),
+        border: OutlineInputBorder(),
+        prefixIconConstraints: BoxConstraints(minWidth: 0, minHeight: 0),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(color: Colors.red, width: 2.0),
         ),
       ),
     );
