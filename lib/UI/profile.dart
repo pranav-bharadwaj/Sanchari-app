5e4cbff66c32c6ba9116755d5518041f093e6dba
pranav-bharadwaj <pranavbharadwaj2001@gmail.com>
2022-07-16 05:34:06 +0530

    Qr added with signin fixed

--- lib/UI/profile.dart
+++ lib/UI/profile.dart
@@ -176,17 +176,10 @@ class Profile extends StatelessWidget {
             ),
             GestureDetector(
               onTap: () => {
-
                 FirebaseAuth.instance.signOut(),
                 Navigator.of(context).pushAndRemoveUntil(
                     MaterialPageRoute(builder: (context) => Login()),
                     (Route<dynamic> route) => false)
-
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Login()),
-                )
-
               },
               child: ProfileListItem(
                 icon: LineAwesomeIcons.alternate_sign_out,
